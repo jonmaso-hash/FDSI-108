1acd87f506e351743516bab68b53fada7aca5ab7
jonmaso-hash <jonmaso@gmail.com>
2026-01-07 05:27:55 -0800

    Add files via upload

---
+++ python-collections.py
@@ -0,0 +1,38 @@
+"""
+Python Collections Comparison Activity
+
+1. Create a list of data structures
+Write down the four main Python collections:
+List
+Dictionary
+Set
+Tuple
+
+2. Provide a syntax example 
+For each collection, write a short Python code example that shows how it looks: 
+List
+Dictionary
+Set 
+Tuple
+
+3. Analyze the characteritistcs 
+Decide for reach collection: 
+Is it ordered (does it keep the order of elements)?
+Is it Mutable (can you change it afer createtion)?
+Does it Allow Duplicates?
+
+4. Identify the access method
+Exaplain how to access elements:
+List/tuple
+Dictionary
+Set
+
+6. Organize the information in a table
+You may create the table in your notebook(on paper) or digitally a text editor, spreadsheet, or Markdown).
+
+ Data Structure| Syntax Example| Ordered| Mutable| Allows Duplicate| Access method
+List           | [10, 20,30]  |  Yes   | Yes       |  Yes           | list[0]
+Dictionary     |{key: "value"}|  yes   |  Yes      |   no           |  ["key"]
+Set            |  (10,20,30)  |  No    |     no    |   no           |[0] iterate
+Tuple          |  (10,20,30)  |  yes   |      no   |    yes         | tuple[0]
+"""
