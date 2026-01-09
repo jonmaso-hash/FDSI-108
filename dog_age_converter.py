fda755f042756998c098e043d5ff68795e550365
jonmaso-hash <jonmaso@gmail.com>
2026-01-08 19:52:53 -0800

    Add files via upload

---
+++ dog_age_converter.py
@@ -0,0 +1,7 @@
+dog_age =  int (input("Enter your dog's age: ")) #int converts string into an interger 
+print(f"Dog's Age {dog_age}") #f-string prints the concat of string and variable for user's output
+
+dog_age =  int (input("Enter your dog's age: ")) #the variable dog age allows user to enter an interger
+print(f"You're dog is  {dog_age *7} years old ") #f-string is concating the string with the variable while multiplying it by 7 of user input
+
+
